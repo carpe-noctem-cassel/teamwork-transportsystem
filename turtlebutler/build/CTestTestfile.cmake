4025944c1bfd47cebc5ea7cbbde7209339042c67
Eric Wagner <e.wagner@volanet.de>
2018-05-31 15:06:47 +0200

    Added a different version of the script created with QTCreator

---
+++ turtlebutler/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/eric/turtlebutler/src
+# Build directory: /home/eric/turtlebutler/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
