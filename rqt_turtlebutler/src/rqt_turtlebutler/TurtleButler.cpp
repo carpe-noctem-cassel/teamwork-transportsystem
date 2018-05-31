36897deba976716d3064ad5782fd60b28fabe5f8
Eric Wagner <e.wagner@volanet.de>
2018-05-31 14:40:52 +0200

    Added incomplete version of a rqt plugin for the Turtlebot Transport UI

---
+++ rqt_turtlebutler/src/rqt_turtlebutler/TurtleButler.cpp
@@ -0,0 +1,50 @@
+#include "rqt_turtlebutler/TurtleButler.h"
+#include <pluginlib/class_list_macros.h>
+#include <QStringList>
+
+namespace rqt_turtlebutler {
+  TurtleButler::TurtleButler()
+    : rqt_gui_cpp::Plugin()
+    , widget(0)
+  {
+    setObjectName("TurtleButler");
+  }
+  
+  void TurtleButler::initPlugin(qt_gui_cpp::PluginContext& context)
+  {
+    QStringList argv = context.argv();
+    widget_ = new QWidget();
+    ui_.setupUi(widget_);
+    context.addWidget(widget_);
+  }
+  
+  void TurtleButler::shutdownPlugin()
+  {
+    // TODO unregister all publishers here
+  }
+  
+  void TurtleButler::saveSettings(qt_gui_cpp::Settings& plugin_settings, qt_gui_cpp::Settings& instance_settings) const
+  {
+    // TODO save intrinsic configurations, usually using:
+    // v = instance_settings.value(k);
+  }
+  
+  void TurtleButler::restoreSettings(const qt_gui_cpp::Settings& plugin_settings, qt_gui_cpp::Settings& instance_settings)
+  {
+    // TODO restore intrinsic configuration, usually using:
+    // v = instance_settings.value(k);
+  }
+  
+  /*
+  bool hasConfiguration() const
+  {
+    return true;
+  }
+  
+  void triggerConfiguration()
+  {
+    // Usually used to open a dialog to offer the user a set of configuration
+  }
+  */
+}
+PLUGINLIB_DECLARE_CLASS(butler, TurtleButler, rqt_turtlebutler::TurtleButler, rqt_gui_cpp::Plugin)
